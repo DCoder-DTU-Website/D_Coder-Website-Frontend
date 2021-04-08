2e64bf8ef71c5274d50aff3ad82656978e6af024
Aaryamann Singh <aaryamannsingh3@gmail.com>
2021-04-09 01:03:00 +0530

    Set up Treact + twin.macro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "style.css"
+import "tailwindcss/dist/base.css"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+import Hero from "components/hero/BackgroundAsImageWithCenteredContent"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AnimationRevealPage disabled>
+      <Hero/>
+    </AnimationRevealPage>
+  )
 }
 
-export default App;
+export default App
