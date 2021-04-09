4af2a024eacb15aed768ea0953d1d4c43ab42468
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-04-09 20:19:58 +0530

    Cards for all the section of Council are Made Separately

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import "style.css"
 import "tailwindcss/dist/base.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Hero from "components/hero/BackgroundAsImageWithCenteredContent"
+import Head from "components/council/heads"
+import CoHead from "components/council/coHeads"
+import Designer from "components/council/designer"
+import Developer from "components/council/developer"
 
 function App() {
   return (
     <AnimationRevealPage disabled>
       <Hero/>
+      <Head />
+      <CoHead />
+      <Designer />
+      <Developer />
     </AnimationRevealPage>
   )
 }
