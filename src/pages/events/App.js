005dee16536ec29cdd1b96b8ca43878590e769de
Naman Malhotra <namalhotra333@gmail.com>
2021-04-10 18:44:02 +0530

    Added events page

--- src/pages/events/App.js
+++ src/pages/events/App.js
@@ -1,14 +1,19 @@
-import React from "react"
-import "style.css"
-import "tailwindcss/dist/base.css"
-import AnimationRevealPage from "helpers/AnimationRevealPage"
-import Hero from "components/hero/SplashScreen"
-import GalPrev from "components/cards/GalPreview"
-import Testimonials from "components/testimonials/HomePageReviews"
+import React from "react";
+import "style.css";
+import "tailwindcss/dist/base.css";
+import AnimationRevealPage from "helpers/AnimationRevealPage";
+import Hero from "components/hero/SplashScreen";
+import GalPrev from "components/cards/GalPreview";
+import Testimonials from "components/testimonials/HomePageReviews";
+import EventPage from "events/EventPage";
+import Footer from "components/footers/Footer";
 
-export default function Council() {
+export default function Events() {
   return (
-    <h1>Now Council!</h1>
+    <>
+      <Hero onClick={(e) => console.log(1)} />
+      <EventPage />
+      <Footer />
+    </>
   );
 }
-
