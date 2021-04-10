9ad4ad964ecc5ff56cfaad53308454e861fdd019
Aaryamann Singh <aaryamannsingh3@gmail.com>
2021-04-10 14:50:22 +0530

    Added Home Page + Set up Routes

---
+++ src/components/cards/Slider.js
@@ -0,0 +1,177 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { SectionHeading } from "components/misc/Headings";
+import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons";
+import { PrimaryLink as PrimaryLinkBase } from "components/misc/Links.js";
+import { ReactComponent as PriceIcon } from "feather-icons/dist/icons/calendar.svg";
+import { ReactComponent as CalendarIcon } from "feather-icons/dist/icons/map-pin.svg";
+import { ReactComponent as StarIcon } from "feather-icons/dist/icons/star.svg";
+import { ReactComponent as ChevronLeftIcon } from "feather-icons/dist/icons/chevron-left.svg";
+import { ReactComponent as ChevronRightIcon } from "feather-icons/dist/icons/chevron-right.svg";
+import {Container as ContainerBase } from "components/misc/Layouts.js"
+import { ReactComponent as ArrowRightIcon } from "images/arrow-right-icon.svg";
+
+const Container = tw(ContainerBase)`bg-gray-900 text-gray-100 -mx-8 -mb-8`
+const Content = tw.div`max-w-screen-xl mx-auto py-16 lg:py-20`;
+const PrimaryLink = styled(PrimaryLinkBase)`
+  ${tw`inline-flex justify-center xl:justify-start items-center mt-8 text-lg mx-10`}
+  svg {
+    ${tw`ml-2 w-5 h-5`}
+  }
+`;
+const HeadingWithControl = tw.div`flex flex-col items-center sm:items-stretch sm:flex-row justify-between p-10`;
+const Heading = tw(SectionHeading)``;
+const Controls = tw.div`flex items-center`;
+const ControlButton = styled(PrimaryButtonBase)`
+  ${tw`mt-4 sm:mt-0 first:ml-0 ml-6 rounded-full p-2`}
+  svg {
+    ${tw`w-6 h-6`}
+  }
+`;
+const PrevButton = tw(ControlButton)``;
+const NextButton = tw(ControlButton)``;
+
+const CardSlider = styled(Slider)`
+  ${tw`mt-16`}
+  .slick-track { 
+    ${tw`flex`}
+  }
+  .slick-slide {
+    ${tw`h-auto flex justify-center mb-1`}
+  }
+`;
+const Card = tw.div`h-full flex! flex-col sm:border-2 sm:border-primary-900 max-w-sm sm:rounded-tl-4xl sm:rounded-br-5xl relative focus:outline-none`;
+const CardImage = styled.div(props => [
+  `background-image: url("${props.imageSrc}");`,
+  tw`w-full h-56 sm:h-64 bg-cover bg-center rounded sm:rounded-none sm:rounded-tl-4xl`
+]);
+
+const TextInfo = tw.div`py-6 sm:px-10 sm:py-6`;
+const TitleReviewContainer = tw.div`flex flex-col sm:flex-row sm:justify-between sm:items-center`;
+const Title = tw.h5`text-2xl font-bold`;
+
+const RatingsInfo = styled.div`
+  ${tw`flex items-center sm:ml-4 mt-2 sm:mt-0`}
+  svg {
+    ${tw`w-6 h-6 text-yellow-500 fill-current`}
+  }
+`;
+const Rating = tw.span`ml-2 font-bold`;
+
+const Description = tw.p`text-sm leading-loose mt-2 sm:mt-4`;
+
+const SecondaryInfoContainer = tw.div`flex flex-col sm:flex-row mt-2 sm:mt-4`;
+const IconWithText = tw.div`flex items-center mr-6 my-2 sm:my-0`;
+const IconContainer = styled.div`
+  ${tw`inline-block rounded-full p-2 bg-gray-700 text-gray-100`}
+  svg {
+    ${tw`w-3 h-3`}
+  }
+`;
+const Text = tw.div`ml-2 text-sm font-semibold text-gray-100`;
+
+const PrimaryButton = tw(PrimaryButtonBase)`mt-auto sm:text-lg rounded-none w-full rounded sm:rounded-none sm:rounded-br-4xl py-3 sm:py-6`;
+export default () => {
+  // useState is used instead of useRef below because we want to re-render when sliderRef becomes available (not null)
+  const [sliderRef, setSliderRef] = useState(null);
+  const sliderSettings = {
+    arrows: false,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  };
+
+  /* Change this according to your needs */
+  const cards = [
+    {
+      imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+      title: "Wyatt Residency",
+      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+      locationText: "Rome, Italy",
+      dateText: "8 April 2021",
+      rating: "4.8",
+    },
+    {
+      imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+      title: "Soho Paradise",
+      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+      locationText: "Ibiza, Spain",
+      dateText: "4 April 2021",
+      rating: 4.9,
+    },
+    {
+      imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+      title: "Hotel Baja",
+      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+      locationText: "Palo Alto, CA",
+      dateText: "3 April 2021",
+      rating: "5.0",
+    },
+    {
+      imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+      title: "Hudak Homes",
+      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
+      locationText: "Arizona, RAK",
+      dateText: "1 April 2021",
+      rating: 4.5,
+    },
+  ]
+
+  return (
+    <Container>
+      <Content>
+        <HeadingWithControl>
+          <Heading>A Sneak Peek</Heading>
+          <Controls>
+            <PrevButton onClick={sliderRef?.slickPrev}><ChevronLeftIcon/></PrevButton>
+            <NextButton onClick={sliderRef?.slickNext}><ChevronRightIcon/></NextButton>
+          </Controls>
+        </HeadingWithControl>
+        <PrimaryLink>
+          Check out our Gallery <ArrowRightIcon />
+        </PrimaryLink>
+        <CardSlider ref={setSliderRef} {...sliderSettings}>
+          {cards.map((card, index) => (
+            <Card key={index}>
+              <CardImage imageSrc={card.imageSrc} />
+              <TextInfo>
+                <TitleReviewContainer>
+                  <Title>{card.title}</Title>
+                </TitleReviewContainer>
+                <SecondaryInfoContainer>
+                  <IconWithText>
+                    <IconContainer>
+                      <CalendarIcon />
+                    </IconContainer>
+                    <Text>{card.locationText}</Text>
+                  </IconWithText>
+                  <IconWithText>
+                    <IconContainer>
+                      <PriceIcon />
+                    </IconContainer>
+                    <Text>{card.dateText}</Text>
+                  </IconWithText>
+                </SecondaryInfoContainer>
+              </TextInfo>
+            </Card>
+          ))}
+        </CardSlider>
+      </Content>
+    </Container>
+  );
+};
---
+++ src/pages/lectures/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "style.css"
+import "tailwindcss/dist/base.css"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+import Hero from "components/hero/SplashScreen"
+import GalPrev from "components/cards/GalPreview"
+import Footer from "components/footers/HomePageFooter"
+import Testimonials from "components/testimonials/HomePageReviews"
+
+export default function Events() {
+  return (
+    <h1>Events here!</h1>
+  );
+}
+
