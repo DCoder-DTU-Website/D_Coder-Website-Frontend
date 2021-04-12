06f278238c48e748e0b1af8c7abefb98dda98d19
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-04-13 01:56:08 +0530

    CoverFlow Apply In Gallery

--- src/pages/gallery/App.js
+++ src/pages/gallery/App.js
@@ -1,14 +1,29 @@
 import React from "react"
-import "style.css"
+// import "App.css"
 import "tailwindcss/dist/base.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
-import Hero from "components/hero/SplashScreenWithHeading"
+import Hero from "components/hero/MinNavbar"
+import Footer from "components/footers/Footer"
+import CoverFlow from "components/Gallery/coverFlow"
 import GalPrev from "components/cards/GalPreview"
 import Testimonials from "components/testimonials/HomePageReviews"
 
 export default function Council() {
   return (
-    <h1>Now Council!</h1>
+    <div>
+      <Hero />
+      <div style = {{
+        width:"100%",
+        height:"100s%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        overflow:"hidden"
+        }}>
+        <CoverFlow />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
