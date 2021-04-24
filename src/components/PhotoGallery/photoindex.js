31a61c838c00e4a27e909938297b7cfb78d89731
Shivansh Dhondiyal <shivanshdhondiyal7723@gmail.com>
2021-04-24 10:32:20 +0400

    Gallery Integration

--- src/components/PhotoGallery/photoindex.js
+++ src/components/PhotoGallery/photoindex.js
@@ -1,16 +1,31 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Gallery from "react-photo-gallery";
 import { photos } from "./photos";
-import Car from "../Gallery/coverFlow"
+import api from "../../api/apiClient";
+import Car from "../Gallery/coverFlow";
 import Carousel, { Modal, ModalGateway } from "react-images";
-import CoverFlow from '../Gallery/coverFlow'
-
+import CoverFlow from "../Gallery/coverFlow";
 
 export default function PhotoIndex() {
+  const [Photos, ImageData] = useState([]);
+  const getGallery = async () => {
+    try {
+      const { data } = await api.get("/gallery/all");
+      const { data: photodata} = data;
+      ImageData(photodata);
+      console.log();
+    } catch (err) {
+      console.log("Could not retrieve Gallery!", err);
+    }
+  };
+
+  useEffect(() => {
+    getGallery();
+  }, []);
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-const openLightbox = useCallback((event, { photo, index }) => {
+  const openLightbox = useCallback((event, { photo, index }) => {
     setCurrentImage(index);
     setViewerIsOpen(true);
   }, []);
@@ -20,25 +35,23 @@ const openLightbox = useCallback((event, { photo, index }) => {
     setViewerIsOpen(false);
   };
   return (
-    <div style = {{
-        padding:"10%",
+    <div
+      style={{
+        padding: "10%",
         // backgroundColor:"rgb(26,32,44)",
-        marginLeft:"-35px",
-        marginRight:"-35px",
-        marginBottom:"-35px"
-    }} >
-      <Gallery photos={photos}  onClick = {openLightbox} />
+        marginLeft: "-35px",
+        marginRight: "-35px",
+        marginBottom: "-35px",
+      }}
+    >
+      <Gallery photos={Photos} onClick={openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
-            <CoverFlow 
-            slideIndex={currentImage}
-            />
-            
+            <CoverFlow slideIndex={currentImage} />
           </Modal>
         ) : null}
       </ModalGateway>
     </div>
   );
 }
-
