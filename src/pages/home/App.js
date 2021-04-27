25058d2b1a7bb3e1fc969d5e6805f9b518e4d987
Naman Malhotra <namalhotra333@gmail.com>
2021-04-27 13:49:28 +0530

    added props support for splash screen

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -12,7 +12,14 @@ import ContactUs from "components/forms/HomePageContact";
 export default function Home() {
   return (
     <>
-      <Hero onClick={() => console.log()} />
+      <Hero
+        onClick={() => console.log()}
+        heading={"D_CODER"}
+        subheading={"Tech Society of DTU"}
+        bgImage={
+          "https://cdn.discordapp.com/attachments/820907278454620183/820908645591678996/ezgif.com-gif-maker_10.gif"
+        }
+      />
       <AboutUs />
       <WhyDCoder />
       <GalPrev />
