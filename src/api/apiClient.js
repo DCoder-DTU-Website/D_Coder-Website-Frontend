f5dbcf0e10e1fcaf6c286762ba4ebed33ef93e8a
Naman Malhotra <namalhotra333@gmail.com>
2021-05-10 23:45:26 +0530

    added backend to heroku

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://dcoderbackend.herokuapp.com/api/",
 });
 
 api.interceptors.request.use(
