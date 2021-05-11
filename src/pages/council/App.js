df0bd345bcadff634aa48cb1169e099bf4c6926b
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-05-11 15:59:16 +0530

    changes in council

--- src/pages/council/App.js
+++ src/pages/council/App.js
@@ -12,6 +12,7 @@ export default function Council() {
         title="The Council"
         bgImage="https://www.therahnuma.com/wp-content/uploads/2018/08/unsc_somalia.jpg"
       />
+      <script type="text/javascript" src="https://form.jotform.com/jsform/211292219446454"></script>
       <Councils />
       <Footer />
     </div>
