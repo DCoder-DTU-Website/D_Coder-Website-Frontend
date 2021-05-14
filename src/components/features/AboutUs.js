f0d433c55eaf6ab2cb8c368210d8cd514e2d3042
Shivansh Dhondiyal <shivanshdhondiyal7723@gmail.com>
2021-05-14 08:40:00 +0400

    AlumniErrors

--- src/components/features/AboutUs.js
+++ src/components/features/AboutUs.js
@@ -12,7 +12,7 @@ import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/users.
 import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/message-square.svg";
 
 const Container = tw.div`relative bg-gray-900 -m-8`;
-const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24 items-center`;
+const TwoColumn = tw.div`flex flex-col md:flex-row justify-between left-0 max-w-screen-xl mx-8 py-20 md:py-24 items-center`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
 const ImageColumn = tw(Column)`md:w-6/12 flex-shrink-0 relative`;
 const TextColumn = styled(Column)((props) => [
