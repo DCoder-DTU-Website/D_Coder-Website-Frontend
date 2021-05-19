a38b76952c6ef65ca2d6ac86b4d3ce30f87e11f4
AdityaTeltia <teltia.aditya22@gmail.com>
2021-05-19 18:03:03 +0530

    Achievements and mission_qabil final

--- src/pages/achievements/App.js
+++ src/pages/achievements/App.js
@@ -3,6 +3,7 @@ import Hero from "components/hero/SplashScreenWithHeading";
 import Footer from "components/footers/Footer";
 import Promotions from "components/achievements/Promotions";
 import Hackathons from "components/achievements/Hackathons";
+import "./App.css";
 
 const App = () => {
   return (
@@ -11,23 +12,14 @@ const App = () => {
         title="COMPETITIONS AND ACHIEVEMENTS"
         bgImage="https://media.giphy.com/media/JUXtbHuixcZKeGJEro/giphy.gif"
       />
-      <p
-        style={{
-          color: "white",
-          textAlign: "center",
-          marginTop: "20px",
-          display: "flex",
-          fontWeight: "bolder",
-          fontSize: "30px",
-        }}
-      >
-        <i>
+      <div style={{ textAlign: "center" }}>
+        <p className="para">
           Continuing the legacy, last year too D_CODER teams participated in
           various competitions and won accolades for the society. Not only the
           members but even for our faculty advisor sir, the year bore many
           gifts.
-        </i>
-      </p>
+        </p>
+      </div>
       <Promotions />
       <Hackathons />
       <Footer />
