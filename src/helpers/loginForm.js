7ef20d161e1a438f8b68de48c84158db7d879fbe
Naman Malhotra <namalhotra333@gmail.com>
2021-08-16 11:13:44 +0530

    revamped login form

--- src/helpers/loginForm.js
+++ src/helpers/loginForm.js
@@ -4,34 +4,27 @@ import styled from "styled-components";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 import api from "../api/apiClient";
 import swal from "sweetalert";
+import { TextField, Button } from "@material-ui/core";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { FaUserAlt, FaLock } from "react-icons/fa";
 
-const Container = tw.div`relative px-10`;
-const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
-const TextColumn = styled(Column)((props) => [
-  tw`md:w-7/12 mt-16 md:mt-0`,
-  props.textOnLeft
-    ? tw`md:mr-12 lg:mr-16 md:order-first`
-    : tw`md:ml-12 lg:ml-16 md:order-last`,
-]);
-
-const TextContent = tw.div`lg:py-8 text-center flex flex-col justify-center items-center md:text-left`;
-
-const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col justify-center items-center max-w-sm mx-auto md:mx-0`;
-const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-blue-600`;
-
-const SubmitButton = tw(
-  PrimaryButtonBase
-)`inline-block text-center bg-blue-600 hocus:bg-blue-800 pt-2`;
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().label("Username").email(),
+  password: Yup.string().required().label("Password"),
+});
 
 const AddForm = ({ handleClose, setIsLoggedIn }) => {
-  const [loginUsername, setLoginUsername] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const login = async () => {
     try {
+      setLoading(true);
+      const info = formik.values;
+      formik.resetForm();
       const { data } = await api.post("/login", {
-        email: loginUsername,
-        username: loginUsername,
-        password: loginPassword,
+        email: info.email,
+        username: info.email,
+        password: info.password,
       });
       localStorage.setItem("token", data);
       const res = await api.get("/user");
@@ -41,53 +34,97 @@ const AddForm = ({ handleClose, setIsLoggedIn }) => {
       } else {
         swal({ title: "Successfully Logged In!", icon: "success" });
       }
+      setLoading(false);
     } catch (err) {
       swal({ title: "Incorrect Username or Password!", icon: "warning" });
     }
   };
 
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: login,
+  });
+
   return (
-    <Container>
-      <div>
-        <TextColumn>
-          <TextContent>
-            <Form>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Input
-                  type="text"
-                  placeholder="Email"
-                  required
-                  onChange={(e) => setLoginUsername(e.target.value)}
-                />
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  required
-                  onChange={(e) => setLoginPassword(e.target.value)}
-                />
-                <br />
-              </div>
-              <SubmitButton
-                onClick={(e) => {
-                  e.preventDefault();
-                  login();
-                  handleClose();
-                }}
-              >
-                Login
-              </SubmitButton>
-            </Form>
-          </TextContent>
-        </TextColumn>
-      </div>
-    </Container>
+    <div style={{ margin: "1em" }}>
+      <form onSubmit={formik.handleSubmit} method="POST">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            style={{
+              margin: "1em",
+              display: "flex",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            <FaUserAlt
+              style={{ position: "absolute", bottom: 10 }}
+              color={formik.errors.email ? "red" : "black"}
+            />
+            <TextField
+              label="Username"
+              id="username"
+              name="email"
+              type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+              fullWidth
+              style={{ marginLeft: "25px" }}
+            />
+          </div>
+          <div
+            style={{
+              margin: "1em",
+              display: "flex",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            <FaLock
+              style={{ position: "absolute", bottom: 10 }}
+              color={formik.errors.password ? "red" : "black"}
+            />
+            <TextField
+              label="Password"
+              id="password"
+              name="password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+              fullWidth
+              style={{ marginLeft: "25px" }}
+            />
+          </div>
+
+          <Button
+            variant="contained"
+            type="submit"
+            style={{
+              margin: "1em",
+              backgroundColor: "#3182ce",
+              color: "white",
+            }}
+            disabled={loading}
+          >
+            Login
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
