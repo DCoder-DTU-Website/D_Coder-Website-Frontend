a73d18e1afc145510fd4c1926c3b44dc9c603114
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-08-20 22:16:16 +0530

    blogs

--- src/components/Blogs/cards/Card.js
+++ src/components/Blogs/cards/Card.js
@@ -1,32 +1,17 @@
 import Button from "@material-ui/core/Button";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import "./Card.css";
 
 function Card({ data, index }) {
   AOS.init();
   return (
     <div
-      style={{
-        margin: "5vh",
-        padding: "5vh",
-        backgroundColor: "#1c1c24",
-        width: "90%",
-        filter: "drop-shadow(10px 10px 4px rgba(0, 0, 0, 0.75))",
-        borderRadius: "5px",
-      }}
       data-aos={index % 2 === 0 ? "fade-left" : "fade-right"}
       data-aos-duration="1000"
+      className="blogCard"
     >
-      <h1
-        style={{
-          color: "#039be5",
-          fontSize: "1.5rem",
-          fontWeight: "Bold",
-          textAlign: "left",
-        }}
-      >
-        {data.title}
-      </h1>
+      <h1 className="blogTitle">{data.title}</h1>
       <hr />
       <div
         style={{
@@ -42,6 +27,7 @@ function Card({ data, index }) {
             color: "white",
             textAlign: "left",
           }}
+          className="blogDesc"
         >
           {data.desc}
         </p>
@@ -53,11 +39,10 @@ function Card({ data, index }) {
               color: "white",
               gap: "5px",
             }}
-          >
-          </div>
+          ></div>
         </div>
       </div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div className = "buttonArea">
         <div style={{ display: "block", margin: "5vh 0 ", fontSize: "1em" }}>
           <div style={{ color: "#53c497", float: "left" }}>
             <div style={{ display: "flex", gap: "20px" }}>{data.author}</div>
@@ -67,8 +52,8 @@ function Card({ data, index }) {
           variant="contained"
           color="primary"
           style={{ margin: "auto 0" }}
-          href = {data.src}
-          target = "_blank"
+          href={data.src}
+          target="_blank"
         >
           Read More
         </Button>
