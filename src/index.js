a3c3ad2b651494cbf007760d5cb4f0647c3d5a6b
Vaibhav Gupta <guptavaibhav432@gmail.com>
2022-01-20 23:23:06 +0530

    Console Removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Notices from "helpers/Notices";
-
+if (process.env.NODE_ENV !== "development") console.log = () => {};
 ReactDOM.render(
   <React.StrictMode>
     <Notices />
