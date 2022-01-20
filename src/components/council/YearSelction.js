e96772b9ab4cbba11c2b82f47d6f671beb3ba990
Naman Malhotra <namalhotra333@gmail.com>
2022-01-20 23:10:56 +0530

    council responsiveness fix

--- src/components/council/YearSelction.js
+++ src/components/council/YearSelction.js
@@ -1,5 +1,6 @@
 import React from "react";
 import tw from "twin.macro";
+import "./YearSelection.css";
 
 const YearDropdown = tw.select`text-gray-900 mx-12 xl:-mx-16 my-2 rounded-md w-48 h-12 px-6 text-lg`;
 const YearItems = tw.option`rounded-md `;
@@ -14,30 +15,33 @@ const Years = [
   },
 ];
 
-class YearSelction extends React.Component{
-  constructor(props){
-    super(props)
-    this.state ={
-      value:"2021-2022",
-      lists: Years
-    }
-     this.handleChange = this.handleChange.bind(this);
+class YearSelction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "2021-2022",
+      lists: Years,
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
   handleChange(e) {
-    this.setState({value:e.target.value})
+    this.setState({ value: e.target.value });
     this.props.setCurrentYear(e.target.value);
   }
 
-  render(){
+  render() {
     return (
-      <YearDropdown style={{ marginLeft: "2rem" }} onChange={this.handleChange}>
+      <YearDropdown
+        className="YearSelection__select"
+        onChange={this.handleChange}
+      >
         {this.state.lists.map((list) => (
           <YearItems key={list.id} value={list.value}>
             {list.value}
           </YearItems>
         ))}
       </YearDropdown>
-    );  
+    );
   }
 }
 
